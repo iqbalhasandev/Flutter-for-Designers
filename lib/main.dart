ac36c701ef709011b6ba0f5706450a79e5255b66
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2021-10-15 08:18:48 +0600

    basic design

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+/* cSpell:disable */
+/* cSpell:disable */
+/* spell-checker: disable */
+/* spellchecker: disable */
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            children: [
+              Container(
+                width: 42.0,
+                height: 42.0,
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14.0),
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFF00AEFF),
+                        Color(0xFF0076FF),
+                      ],
+                    )),
+                child: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Container(
+                child: Text(
+                  'Home',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w800,
+                    color: Color(0xFF242629),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
